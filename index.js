93589bc6bf1a1c5f6ebaf6064723dce975c52ab7
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-10-24 09:29:01 +0700

    middleware

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import bodyParser from "body-parser";
+import session from "express-session";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Halo Dunia!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is about");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("This is contact");
+});
+
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Menampilkan user ID ${id}`);
+});
+
+app.post("/submit", (req, res) => {
+  res.send("Data berhasil dikirim");
 });
 
 app.listen(PORT, () => {
